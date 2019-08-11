267ddf94eaae5a2ce6733801284ec91503b37e01
Jordan Finch <jordan.finch@student.uts.edu.au>
2019-08-11 19:06:24 +1000

    Add WIP page components, domain models and service for discussion posts to front-end

---
+++ pictochat-fe/src/model/PostAuthor.ts
@@ -0,0 +1,19 @@
+
+export interface PostAuthor {
+    userName: string;
+}
+
+// export class PostAuthor {
+//     userName: string;
+//     constructor(userName: string) {
+//         this.userName;
+//     }
+
+//     /**
+//      * @param obj JSON like object with structure matching instance attributes. 
+//      *      All properties should be string.
+//      */
+//     static fromJSON(obj: any): PostAuthor {
+//         return new PostAuthor(obj.userName);
+//     }
+// }
