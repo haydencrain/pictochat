9d84ccd09fda7e3e9d86615dc157621cc5d402be
Hayden Crain <haydencrain@optusnet.com.au>
2019-08-16 00:31:35 +1000

    US-119 add page routes components

--- pictochat-fe/src/components/App/App.tsx
+++ pictochat-fe/src/components/App/App.tsx
@@ -1,13 +1,24 @@
 import * as React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from '../common/Navbar';
+import HomePage from '../pages/Home';
+import NotFoundPage from '../pages/NotFound';
+import DiscussionPage from '../pages/Discussion';
+import LeaderboardPage from '../pages/Leaderboard';
+import RegisterPage from '../pages/Register';
 import './App.less';
 
 export default () => (
   <BrowserRouter>
     <Navbar />
     <main id="main-content">
-      <Switch></Switch>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/discussion" component={DiscussionPage} />
+        <Route exact path="/leaderboard" component={LeaderboardPage} />
+        <Route exact path="/register" component={RegisterPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </main>
   </BrowserRouter>
 );
