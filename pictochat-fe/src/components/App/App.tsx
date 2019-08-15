b8bb580925dc339ad1ecfb63e225b0ca2bf2fd2e
Hayden Crain <haydencrain@optusnet.com.au>
2019-08-15 23:35:38 +1000

    US-119 add semantic ui and react router, set up basic app component, add navbar

--- pictochat-fe/src/components/App/App.tsx
+++ pictochat-fe/src/components/App/App.tsx
@@ -1,56 +1,13 @@
 import * as React from 'react';
-import duck from '../../images/duck.jpg';
-import SignUp from '../SignUp/SignUp';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import Navbar from '../common/Navbar';
 import './App.less';
 
-interface TestProps {
-  header: string;
-}
-
-interface IState {
-  apiResponse: string;
-}
-
-interface IProps { }
-
-const Test = (props: TestProps) => (
-  <div className="test">
-    <h1>{props.header}</h1>
-    <img src={duck} />
-  </div>
+export default () => (
+  <BrowserRouter>
+    <Navbar />
+    <main id="main-content">
+      <Switch></Switch>
+    </main>
+  </BrowserRouter>
 );
-
-class App extends React.Component<IProps, IState> {
-  static readonly API_ROOT: string = process.env.PICTOCHAT_API_ROOT;
-
-  constructor(props: IProps) {
-    super(props as any);
-    this.state = { apiResponse: "" };
-  }
-
-  private callApi() {
-    fetch(App.API_ROOT + '/test')
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }))
-      .catch(err => err);
-  }
-
-  componentDidMount() {
-    this.callApi();
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <Test header="Hello World!" />
-        <SignUp />
-        <br />
-        <p>{this.state.apiResponse}</p>
-      </div>
-    );
-  }
-}
-
-
-
-export default App;
