c650bc12d61ab0073555ef5abac72b08fc5b3ecd
rach_coster <rachel.coster@outlook.com>
2019-08-18 19:09:01 +1000

    Completed the leaderboard

---
+++ pictochat-fe/src/components/common/Leaderboard/Leaderboard.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {Container, Header, Image, Rating} from 'semantic-ui-react';
+
+
+export default (props: {}) => (
+  <Container Leaderboard>
+    <Header as='h2'>Leaderboard</Header>
+    <Image src='./images/elyse.png' size='tiny' verticalAlign='top' circular/> <p> Doss </p>
+  </Container>
+);
