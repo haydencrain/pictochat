74c2f50a0e8f5618c6f33212996be91720860fd4
Jordan Finch <jordan.finch@student.uts.edu.au>
2019-08-20 13:16:54 +1000

    Add links for threads and leaderboard page to navbar.

--- pictochat-fe/src/components/common/Navbar/Navbar.tsx
+++ pictochat-fe/src/components/common/Navbar/Navbar.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.less';
 
 export default (props: {}) => (
   <nav id="navbar">
-    <div className="title">Pictochat</div>
+    <div className="title">
+      Pictochat
+      <span className="nav-links-container">
+        <Link to="/threads">Threads</Link>
+        <Link to="/leaderboard">Leaderboard</Link>
+      </span>
+    </div>
   </nav>
 );
