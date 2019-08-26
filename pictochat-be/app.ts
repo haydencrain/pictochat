178369b5f899311bbab909916f6996da9b444dae
Jordan Finch <jordan.finch@student.uts.edu.au>
2019-08-26 20:59:28 +1000

    Fix redirection bug by removing wildcard '*' route from front-end-route.ts in backend.

--- pictochat-be/app.ts
+++ pictochat-be/app.ts
@@ -19,6 +19,9 @@ console.log('WEB_CONTENT_DIR: ' + WEB_CONTENT_DIR);
 
 const app = express();
 
+
+/// MIDDLEWARE ///
+
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', FRONTEND_REQUEST_ORIGIN);
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
@@ -33,36 +36,32 @@ app.use(function (req, res, next) {
   next();
 });
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
-// FRONTEND ROUTE
+
+/// ROUTES ///
+
+// Frontend
 const frontEndRouter = makeFrontEndRouter(WEB_CONTENT_DIR);
 app.use('/', frontEndRouter);
 
-
-// API ROUTE
+// API
 app.use('/api', apiRouter);
 
-
-//app.use('/', indexRouter);
-// app.use('/user', usersRouter);
-// app.use('/testAPI', testAPIRouter);
-
+// Enable client-side routing
+app.use('*', (req, res) => res.sendFile(path.join(WEB_CONTENT_DIR, 'index.html')));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
+/// ERROR HANDLERS ///
+
 // app.use(function (err, req, res, next) {
 //   // set locals, only providing error in development
 //   res.locals.message = err.message;
