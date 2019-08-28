14c074e1dc817e606059a96e9d2780967b695ab5
Hayden Crain <haydencrain@optusnet.com.au>
2019-08-28 10:58:04 +1000

    US-102 remove mistakenly added code

--- pictochat-fe/src/services/DiscussionService.ts
+++ pictochat-fe/src/services/DiscussionService.ts
@@ -12,9 +12,3 @@ export class DiscussionService {
 }
 
 export default new DiscussionService();
-
-function u(t) {
-  for (i = a = c.width = 2e3; i--; x.fillRect(a, 0, i / 3, a))
-    x.transform(1, -i / 5e5, (n = 1 + C(t + i) * 4), 1, n / i - 199, 0),
-      (x.fillStyle = R(i - a / 2, (b = i / 9 + 99 * C(t)), b, 0.04));
-}
