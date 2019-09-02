002eae91b53c5fc3e8765b8de105edfad6f0c344
Jordan Finch <jordan.finch@student.uts.edu.au>
2019-09-02 23:11:35 +1000

    Add commentCount to root the DiscussionTreeNode returned by /discussions/:id

--- pictochat-be/models/discussion-tree-node.ts
+++ pictochat-be/models/discussion-tree-node.ts
@@ -5,10 +5,17 @@ import { DiscussionPost } from './discussion-post';
  */
 export class DiscussionTreeNode extends DiscussionPost {
   replies: DiscussionTreeNode[];
+  commentCount?: number; // Only populated for the root node
 
-  constructor(post: DiscussionPost) {
+  /**
+   * For internal use only!
+   */
+  private constructor(post: DiscussionPost, commentCount?: number) {
     super(post.toJSON());
     this.replies = [];
+    if (commentCount !== undefined) {
+      this.commentCount = commentCount;
+    }
   }
 
   addReply(reply: DiscussionTreeNode) {
@@ -17,6 +24,20 @@ export class DiscussionTreeNode extends DiscussionPost {
 
   toJSON(): object {
     let baseJSON = super.toJSON();
-    return {...baseJSON, ...{replies: this.replies}};
+    let repliesJSON = this.replies.map((reply) => reply.toJSON());
+    baseJSON = { ...baseJSON, ...{ replies: repliesJSON } };
+    if (this.commentCount) {
+      baseJSON['commentCount'] = this.commentCount;
+    }
+    return baseJSON;
+  }
+
+  // Factory
+  static async makeInstance(post: DiscussionPost): Promise<DiscussionTreeNode> {
+    let commentCount: number = null;
+    if (post.isRootPost) {
+      commentCount = await post.getDeepReplyCount();
+    }
+    return new DiscussionTreeNode(post, commentCount);
   }
 }
