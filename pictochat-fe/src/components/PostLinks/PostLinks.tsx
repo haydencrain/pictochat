7ef84dd82b76bca879540897b4f421b115a7b79f
Jordan Finch <jordan.finch@student.uts.edu.au>
2019-09-13 00:01:31 +1000

    Fix parentPostId bug in front-end PostLinks component so that replies are made to selected the post, not its parent.

--- pictochat-fe/src/components/PostLinks/PostLinks.tsx
+++ pictochat-fe/src/components/PostLinks/PostLinks.tsx
@@ -11,8 +11,8 @@ interface PostLinksProps {
 }
 
 export default function PostLinks(props: PostLinksProps) {
-  const { postType, post } = props;
-  const { postId, commentCount, parentPostId } = post;
+  // const { postType, post } = props;
+  // const { postId, commentCount, parentPostId } = post;
 
   const mapLinks = (links: JSX.Element[]) =>
     links.map((link, index) => (
@@ -23,8 +23,8 @@ export default function PostLinks(props: PostLinksProps) {
 
   const renderRootPostLinks = () => {
     const rootPostLinks = [
-      <Link className="link" to={`/discussion?id=${postId}`}>
-        {commentCount} comments
+      <Link className="link" to={`/discussion?id=${props.post.postId}`}>
+        {props.post.commentCount} comments
       </Link>
     ];
     return mapLinks(rootPostLinks);
@@ -32,17 +32,17 @@ export default function PostLinks(props: PostLinksProps) {
 
   const renderReplyPostLinks = () => {
     const replyPostLinks = [
-      <Link className="link" to={`/discussion?id=${postId}`}>
+      <Link className="link" to={`/discussion?id=${props.post.postId}`}>
         permalink
       </Link>,
-      <CreatePostModal triggerType="link" triggerContent="reply" parentId={parentPostId} />
+      <CreatePostModal triggerType="link" triggerContent="reply" parentId={props.post.postId} />
     ];
     return mapLinks(replyPostLinks);
   };
 
   const renderLinks = () => {
-    if (postType === PostTypes.Root) return renderRootPostLinks();
-    if (postType === PostTypes.Reply) return renderReplyPostLinks();
+    if (props.postType === PostTypes.Root) return renderRootPostLinks();
+    if (props.postType === PostTypes.Reply) return renderReplyPostLinks();
     return null; // No Links should be shown on the Main Post
   };
 
