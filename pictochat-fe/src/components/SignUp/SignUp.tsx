2325cd00dcef22b2542bd08ebe5330f5ef75af90
rach_coster <rachel.coster@outlook.com>
2019-09-16 16:33:04 +1000

    Register complete with cancel clearing the form

--- pictochat-fe/src/components/SignUp/SignUp.tsx
+++ pictochat-fe/src/components/SignUp/SignUp.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Container, Header, Form, Button } from 'semantic-ui-react';
+import { Container, Header, Form, Button, Divider } from 'semantic-ui-react';
 import UserService from '../../services/UserService';
 import './Signup.less';
 import { User } from '../../models/User';
@@ -10,6 +10,7 @@ interface SignUpState {
   username: string;
   email: string;
   password: string;
+  retryPwd: string;
 }
 
 class SignUp extends React.Component<{}, SignUpState> {
@@ -18,13 +19,15 @@ class SignUp extends React.Component<{}, SignUpState> {
     this.state = {
       username: '',
       email: '',
-      password: ''
+      password: '',
+      retryPwd: ''
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleUsernameChange = this.handleUsernameChange.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleRetryPwdChange = this.handleRetryPwdChange.bind(this);
   }
 
   handleUsernameChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -39,6 +42,20 @@ class SignUp extends React.Component<{}, SignUpState> {
     this.setState({ password: event.target.value });
   }
 
+  handleRetryPwdChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ retryPwd: event.target.value });
+  }
+
+  clearForm = (event: any) => {
+    event.preventDefault();
+    this.setState({
+      username: '',
+      email: '',
+      password: '',
+      retryPwd: ''
+    });
+  };
+
   async handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const user: User = {
@@ -46,11 +63,10 @@ class SignUp extends React.Component<{}, SignUpState> {
       email: this.state.email,
       password: this.state.password
     };
-
+    console.log(user);
     const res = await UserService.addUser(user);
     console.log(res);
   }
-
   render() {
     return (
       <Container id="register-container">
@@ -60,40 +76,34 @@ class SignUp extends React.Component<{}, SignUpState> {
         <Form id="register-form" onSubmit={this.handleSubmit} method="POST">
           <Form.Field className="register-field">
             <label>Username</label>
-            <input
-              type="text"
-              placeholder="username"
-              value={this.state.username}
-              onChange={this.handleUsernameChange}
-            />
+            <input type="text" value={this.state.username} onChange={this.handleUsernameChange} />
           </Form.Field>
           <Form.Field className="register-field">
             <label>Email</label>
-            <input type="text" placeholder="email" value={this.state.email} onChange={this.handleEmailChange} />
+            <input type="text" value={this.state.email} onChange={this.handleEmailChange} />
           </Form.Field>
+          <Divider hidden />
           <Form.Field className="resgister-field">
             <label>Password</label>
             <input type="password" value={this.state.password} onChange={this.handlePasswordChange} />
           </Form.Field>
-          <Form.Field>
-            <Button primary className="register-button">
-              Register
-            </Button>
+          <Form.Field className="resgister-field">
+            <label>Re-type Password</label>
+            <input type="password" value={this.state.retryPwd} onChange={this.handleRetryPwdChange} />
           </Form.Field>
+          <Divider hidden />
+          <Form.Group widths="equal">
+            <Form.Field>
+              <Button onClick={this.clearForm}>Cancel</Button>
+            </Form.Field>
+            <Form.Field className="signup-button">
+              <Button primary>Sign Up</Button>
+            </Form.Field>
+          </Form.Group>
         </Form>
       </Container>
     );
   }
 }
-
-// const SignUp = () => (
-//   <form>
-//     Username: <input className="user" type="text" />
-//     <br />
-//     Password: <input className="pwd" type="text" />
-//     <br />
-//     <input className="submit" type="submit" />
-//   </form>
-// );
 
 export default SignUp;
