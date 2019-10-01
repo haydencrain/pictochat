eab5e2cb18f0d72b243c83f15d2ce613e15f155d
rach_coster <rachel.coster@outlook.com>
2019-10-01 13:02:23 +1000

    Reactions front end inital hookup to back end

--- pictochat-fe/src/components/Reactions/Reactions.tsx
+++ pictochat-fe/src/components/Reactions/Reactions.tsx
@@ -1,43 +1,72 @@
 import * as React from 'react';
 import { Button, Popup, Label } from 'semantic-ui-react';
-import { useBooleanKnob } from '@stardust-ui/docs-components';
 import { useFetchReactions } from '../../hooks/ReactionHooks';
 import reactionService from '../../services/ReactionService';
 import './Reactions.less';
 import StoresContext from '../../contexts/StoresContext';
+import { useToggleModal } from '../../hooks/ModalHooks';
 
-const reactions = [{ icon: '👍' }, { icon: '👎' }, { icon: '😂' }, { icon: '😍' }, { icon: '😡' }];
-const stores = React.useContext(StoresContext);
-const currentUser = stores.user.currentUser;
+const REACTIONS = [{ icon: '👍' }, { icon: '👎' }, { icon: '😂' }, { icon: '😍' }, { icon: '😡' }];
 
-const AddReaction = () => {
-  const [eventEnabled] = useBooleanKnob({
-    name: 'eventEnabled',
-    initialValue: true
-  });
+interface ReactionsProps {
+  postId: number;
+}
 
-  const [open, setOpen] = useBooleanKnob({ name: 'open' });
+function Reactions(props: ReactionsProps) {
+  const stores = React.useContext(StoresContext);
+  const currentUser = stores.user.currentUser;
+
+  const [reactions, isLoading] = useFetchReactions(props.postId);
+
+  //TODO: arrange by reaction and then get the subsequent counts
+
+  const addReaction = (reactionName: string) => {};
+
+  const currentReactions = REACTIONS.map((react, index) => (
+    <Label key={index} onClick={() => addReaction('name')}>
+      <p className="icon"> {react.icon}</p> <p>1</p>
+    </Label>
+  ));
+
+  return (
+    <section className="reactions">
+      <PopupReactions increaseReactionCount={addReaction} />
+      <div className="curr-react">{currentReactions}</div>
+    </section>
+  );
+}
+
+interface ReactionPopUpProps {
+  increaseReactionCount: (reactionName: string) => void;
+}
+
+function PopupReactions(props: ReactionPopUpProps) {
+  const { isActive, onOpen, onClose } = useToggleModal();
+
+  const handleClickedReaction = (reactionName: string) => {
+    props.increaseReactionCount(reactionName);
+  };
+
+  const content = (
+    <ul>
+      <li onClick={() => handleClickedReaction('thumbs-up')}>REACTIONS.icon[0]</li>
+      <li onClick={() => handleClickedReaction('thumbs-down')}>REACTIONS.icon[1]</li>
+      <li onClick={() => handleClickedReaction('laugh')}>REACTIONS.icon[2]</li>
+      <li onClick={() => handleClickedReaction('heart')}>REACTIONS.icon[3]</li>
+      <li onClick={() => handleClickedReaction('angry')}>REACTIONS.icon[4]</li>
+    </ul>
+  );
 
   return (
     <Popup
-      content="👍 👎 😂 😍 😡"
-      eventEnabled={eventEnabled}
+      content={content}
+      open={isActive}
+      onOpen={onOpen}
+      onClose={onClose}
       on="click"
-      onOpen={() => setOpen(true)}
       trigger={<Button icon="add" />}
     />
   );
-};
+}
 
-const CurrentReactions = reactions.map((react, index) => (
-  <Label key={index}>
-    <p className="icon">{react.icon}</p> <p>1</p>
-  </Label>
-));
-
-export default () => (
-  <section className="reactions">
-    <AddReaction />
-    <div className="curr-react">{CurrentReactions}</div>
-  </section>
-);
+export default Reactions;
