16b08d4f96ac8d3b4af9b5d50fd6f7f2efe988d6
Jordan Finch <jordan.finch@student.uts.edu.au>
2019-10-05 17:44:45 +1000

    Use UserService createUser method rather than user models method when creating admin user (so password is hashed).

--- pictochat-be/utils/create-admin-user.ts
+++ pictochat-be/utils/create-admin-user.ts
@@ -1,12 +1,14 @@
-import { User } from '../models/user';
+import { UserService } from '../services/user-service';
 import { SequelizeConnectionService } from '../services/sequelize-connection-service';
 import config from './config';
 
 export async function createAdminUser() {
   const sequelize = SequelizeConnectionService.getInstance();
   await sequelize.transaction(async transaction => {
-    let admin = await User.createUser(config.ADMIN_USER, config.ADMIN_PASSWORD);
+    let admin = await UserService.createUser(config.ADMIN_USER, config.ADMIN_PASSWORD);
     admin.hasAdminRole = true;
+    // FIXME: Figure out if email actually needs to be set
+    admin.email = 'admin@example.com';
     await admin.save();
   });
 }
