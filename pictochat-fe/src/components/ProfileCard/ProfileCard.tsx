9012ca735399893913303c6aa6120bd49db6ed91
Hayden Crain <haydencrain@optusnet.com.au>
2019-10-08 03:10:05 +1100

    remove hardcoded scores and just leave blank for now

--- pictochat-fe/src/components/ProfileCard/ProfileCard.tsx
+++ pictochat-fe/src/components/ProfileCard/ProfileCard.tsx
@@ -45,19 +45,19 @@ function ProfileCard(props: Props) {
           <div className="user-name">{props.user.username}</div>
           <div className="user-rating">
             <label>Rating</label>
-            <span className="value">4.05</span>
-            <Rating className="rating-stars" icon="star" defaultRating={4.05} maxRating={5} disabled />
+            <span className="value">0.00</span>
+            <Rating className="rating-stars" icon="star" defaultRating={0} maxRating={5} disabled />
           </div>
         </div>
       </Segment>
       <Segment className="profile-card-body">
         <div className="user-statistic">
           <label>Thread Score</label>
-          <span className="value">295</span>
+          <span className="value">n/a</span>
         </div>
         <div className="user-statistic">
           <label>Comment Score</label>
-          <span className="value">35</span>
+          <span className="value">n/a</span>
         </div>
       </Segment>
       {linksSegment}
