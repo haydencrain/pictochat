d37e150cb91922a623d711f7b2286fbe9f8c1a62
Hayden Crain <haydencrain@optusnet.com.au>
2019-10-08 12:17:09 +1100

    allow users to report a post

--- pictochat-fe/src/services/ContentReportService.ts
+++ pictochat-fe/src/services/ContentReportService.ts
@@ -1,9 +1,18 @@
-import { IDiscussionPost } from '../models/DiscussionPost';
+import { IDiscussionPost, DiscussionPost } from '../models/DiscussionPost';
 import ApiService from './ApiService';
 
 export class ContentReportService {
   static async getContentReports(): Promise<IDiscussionPost[]> {
     return await ApiService.get('/content-report');
+  }
+
+  static async reportPost(
+    postId: string | number
+  ): Promise<{
+    postId: any;
+    hasInappropriateContentFlag: any;
+  }> {
+    return await ApiService.post(`/post/${postId}/content-report`, {});
   }
 
   static async unflagReportedPost(
