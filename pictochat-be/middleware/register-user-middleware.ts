16aaf9882c29b7cb1d424ea0f8fa63a153f686e9
rach_coster <rachel.coster@outlook.com>
2019-10-15 12:09:56 +1100

    JSDoc complete for middleware

--- pictochat-be/middleware/register-user-middleware.ts
+++ pictochat-be/middleware/register-user-middleware.ts
@@ -1,9 +1,11 @@
 import * as EmailValidator from 'email-validator';
 import { UnprocessableError } from '../exceptions/unprocessable-error';
-
 /**
  * Use this to check the field validation of the user register form
- *  */
+ * @param req
+ * @param res
+ * @param next
+ */
 export async function registerUserMiddleware(req, res, next) {
   const { username, email, password } = req.body;
   if (!EmailValidator.validate(email)) {
