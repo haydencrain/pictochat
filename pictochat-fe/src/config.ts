14241ee2603d95adcda8d2c851277ccbad7c7603
Jordan Finch <jordan.finch@student.uts.edu.au>
2019-10-21 02:33:06 +1100

    Make '/' default for PICTOCHAT_API_ROOT

--- pictochat-fe/src/config.ts
+++ pictochat-fe/src/config.ts
@@ -1,7 +1,7 @@
 export default {
   urls: {
     FRONTEND_URL_ROOT: process.env.PICTOCHAT_FRONTEND_URL_ROOT || '/',
-    BACKEND_ENDPOINT: process.env.PICTOCHAT_API_ROOT || 'http://localhost:443/api'
+    BACKEND_ENDPOINT: process.env.PICTOCHAT_API_ROOT || '/'
   },
   discussion: {
     PAGINATION_LIMIT: process.env.DISCUSSION_PAGINATION_LIMIT ? parseInt(process.env.DISCUSSION_PAGINATION_LIMIT) : 10
